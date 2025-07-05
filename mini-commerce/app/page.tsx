21896fad756849c1197ba1b2b247c1345fc11c31
emmanuel <esongz456@gmail.com>
2025-07-05 05:54:18 +0100

    updates

--- mini-commerce/app/page.tsx
+++ mini-commerce/app/page.tsx
@@ -1,9 +1,10 @@
-"use client";
+'use client';
 import React from "react";
 import FooterBanner from "@/components/FooterBanner";
 import HeroBanner from "@/components/HeroBanner";
 import ProductCard from "@/components/ProductCard";
 import useProducts from "@/hooks/useProduct";
+import type { Product } from '@/types/product';
 
 export default function Home() {
   const { data: products, isLoading, error } = useProducts();
@@ -14,36 +15,36 @@ export default function Home() {
 
   return (
     <div>
-      <HeroBanner
-        headline="Headphones"
-        subheadline="Wireless"
-        productName="Beats Solo"
-        description="Premium wireless headphones with noise cancellation"
-        buttonText="Shop Now"
-        imageUrl="https://file.aiquickdraw.com/imgcompressed/img/compressed_116f0026448d12885a4eeaf7a883205b.webp"
+      <HeroBanner 
+        headline="Headphones" 
+        subheadline="Wireless" 
+        productName="Beats Solo" 
+        description="Premium wireless headphones with noise cancellation" 
+        buttonText="Shop Now" 
+        imageUrl="https://file.aiquickdraw.com/imgcompressed/img/compressed_116f0026448d12885a4eeaf7a883205b.webp" 
       />
       <div className="products-heading">
         <h1>Best selling products</h1>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {products.map((product: any) => (
+        {products.map((product: Product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
-      <FooterBanner   discount="20% off"
-  largeText1="Summer Sale"
-  largeText2="Hot Deals"
-  saleTime="Limited time offer"
-  smallText="Don't miss out"
-  midText="Get ready to save"
-  product="beats"
-  buttonText="Shop Now"
-  imageUrl="https://file.aiquickdraw.com/imgcompressed/img/compressed_09f664a19dde696df53ee167f7d2a8a4.webp"
-  desc="Description of the product"
-/>
+      <FooterBanner 
+        discount="20% off" 
+        largeText1="Summer Sale" 
+        largeText2="Hot Deals" 
+        saleTime="Limited time offer" 
+        smallText="Don't miss out" 
+        midText="Get ready to save" 
+        product="beats" 
+        buttonText="Shop Now" 
+        imageUrl="https://file.aiquickdraw.com/imgcompressed/img/compressed_09f664a19dde696df53ee167f7d2a8a4.webp" 
+        desc="Enjoy exclusive summer deals on our best products!" 
+      />
     </div>
   );
 }
-
 
